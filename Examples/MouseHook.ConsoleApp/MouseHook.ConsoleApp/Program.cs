de511f8b66b166b076eb2ae9022b6f85b26d5cf3
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2020-05-19 20:28:22 +0200

    - added example project for mousehook assembly

---
+++ Examples/MouseHook.ConsoleApp/MouseHook.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BK.Plugins.MouseHook.Core;
+using BK.Plugins.MouseHook.Logic;
+using System.Windows.Forms;
+
+namespace MouseHook.ConsoleApp
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			var hook = new MouseHookLogic();
+			hook.MouseHookEvent += HookOnMouseHookEvent;
+			hook.SetHook();
+			
+			Application.Run();
+			
+		}
+
+		private static void HookOnMouseHookEvent(object sender, MouseParameter e)
+		{
+			
+		}
+	}
+}
